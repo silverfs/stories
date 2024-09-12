4a947699cdac10bf63af1104dde431bd79469e29
SilverFS <45943209+silverfs@users.noreply.github.com>
2024-09-12 20:00:14 +0200

    Added new project for recipes

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -4,7 +4,6 @@ interface Project {
   href?: string
   imgSrc?: string
 }
-
 const projectsData: Project[] = [
   {
     title: 'Lychee',
@@ -17,6 +16,12 @@ const projectsData: Project[] = [
     description: `Mostly for personal use, but maybe some info might be of use to you as well. Things I learn and are in need to be written down in regards to tech will be written down here.`,
     imgSrc: '/static/images/project/wiki-shiruvaaa',
     href: 'https://wiki.shiruvaaa.net',
+  },
+  {
+    title: 'EHBO (WIP)',
+    description: `EHBO: "Eerste Hulp Bij Ondervoeding" -- Or in English: "First Aid For Malnutrition" | A cozy source for good recipes.`,
+    imgSrc: 'https://lychee.shiruvaaa.net/uploads/medium/7e/e9/2bd722254ecd085670df5541aff9.jpg',
+    href: '/404',
   },
 ]
 
